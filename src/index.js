888bd6892f976fe092ddf381181116e4e8e7db85
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-22 16:24:25 -0500

    Add provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import { REGION } from './modules/environment';
+import store from './redux/configureStore';
+import { getCountries } from './redux/countries/countries';
+
+store.dispatch(getCountries(REGION));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
