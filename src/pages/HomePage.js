64cb1058b85f5a00cd23f081011a6a98eed0aadd
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-22 16:26:49 -0500

    Add HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,25 @@
+import { shallowEqual, useSelector } from 'react-redux';
+import CountryList from '../components/CountryList';
+
 const HomePage = () => {
-  const page = 'HomePage';
+  const countryList = useSelector((state) => state.countries, shallowEqual);
+  let filtered = countryList;
+
+  const onChange = (event) => {
+    const text = (event.target.value || '').trim();
+    console.log(text);
+    if (text) {
+      filtered = countryList.filter((country) => country.name.toLowerCase().includes(text));
+      console.log(filtered);
+    }
+  };
+
   return (
     <div>
-      {page}
-      {' '}
-      works!
+      <div className="filter">
+        <input type="text" placeholder="Filter country list" onChange={onChange} />
+      </div>
+      <CountryList countries={filtered} />
     </div>
   );
 };
