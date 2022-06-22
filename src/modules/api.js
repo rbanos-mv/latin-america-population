e0c40f400c43999a4d3e5eb7e5a7f05d3fbcaad4
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-22 16:23:15 -0500

    Fetch Countries from API

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,1 +1,15 @@
-// import API_URL from './environment';
+import { API_URL } from './environment';
+
+const apiGetCountries = (region) => fetch(`${API_URL}/country?format=json&region=${region}`)
+  .then((response) => {
+    const responseLength = Number(response.headers.get('content-length'));
+    if (responseLength > 0) {
+      return response.json();
+    }
+    return [];
+  })
+  .catch((error) => {
+    throw error;
+  });
+
+export default apiGetCountries;
