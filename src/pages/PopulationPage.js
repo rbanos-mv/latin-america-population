2471d1f7f0e40ebee2373bacf945b4fce7bb9ab2
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-23 10:06:10 -0500

    Create PopulationPage

--- src/pages/PopulationPage.js
+++ src/pages/PopulationPage.js
@@ -1,11 +1,48 @@
+import { useEffect, useState } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { FLAGS_URL } from '../modules/environment';
+// import Country from '../components/Country';
+import PopulationList from '../components/PopulationList';
+import { getPopulation } from '../redux/countries/countries';
+
 const PopulationPage = () => {
-  const page = 'PopulationPage';
+  const [isReady, setIsReady] = useState(false);
+  const [country, setCountry] = useState(null);
+
+  const { idCountry = 'xxx' } = useParams();
+  const dispatch = useDispatch();
+  const countryList = useSelector((state) => state.countries, shallowEqual);
+
+  useEffect(() => {
+    if (countryList.length) {
+      setIsReady(true);
+      const country = countryList.find((item) => item.id === idCountry);
+      if (country.population.length === 0) {
+        dispatch(getPopulation(idCountry));
+      }
+    }
+  }, [countryList.length]);
+
+  if (countryList.length && isReady && country === null) {
+    const country = countryList.find((item) => item.id === idCountry);
+    if (country !== null && country.population.length) {
+      setCountry(country);
+    }
+  }
+
   return (
-    <div>
-      {page}
-      {' '}
-      works!
-    </div>
+    country && (
+      <div className="column">
+        <div className="card flag">
+          <img src={`${FLAGS_URL}/${country.id.toLowerCase()}.svg`} alt="Flag" />
+          <h2>{country.name}</h2>
+        </div>
+        {/* <div className="column flag"> */}
+        <PopulationList population={country.population} />
+        {/* </div> */}
+      </div>
+    )
   );
 };
 
