969cc1b74e3c4801027134d82d0897d7adebf35b
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-23 10:11:24 -0500

    Remove StrictMode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -10,9 +9,7 @@ store.dispatch(getCountries(REGION));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
